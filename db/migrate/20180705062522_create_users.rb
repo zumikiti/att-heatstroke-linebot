a25195ec6d26c0515c31733f1cd48abc35d32644
zumikiti <aponmaru@gmail.com>
2018-07-07 19:29:06 +0900

    reset

--- db/migrate/20180705062522_create_users.rb
+++ db/migrate/20180705062522_create_users.rb
@@ -1,6 +1,6 @@
-class CreateUser < ActiveRecord::Migration[5.2]
+class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :user do |t|
+    create_table :users do |t|
       t.string :line_id, null: false
 
       t.timestamps
